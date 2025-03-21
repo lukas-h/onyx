63223121f6bb0e04cf5e1f3d3f0f2189e72142b2
Ryan Comerford <hello@ryancomerford.me>
2025-03-21 13:02:19 +1100

    Fix parser null and number duplication bugs by improving regex and minimising edge cases

--- lib/editor/parser.dart
+++ lib/editor/parser.dart
@@ -1,6 +1,15 @@
 import 'package:onyx/editor/model.dart';
 
-final expression = RegExp(r'^\:[0-9]+(([\.\,])+[0-9]+)?');
+final mathematicalExpressionRegex =
+    RegExp(r'^(?<op>:[=+-/*]?)(?<num>[0-9]+([,.]?[0-9]+)?)?(?<text>.*)');
+
+final operators = {
+  ':-': Operator.subtract,
+  ':+': Operator.add,
+  ':/': Operator.divide,
+  ':*': Operator.multiply,
+  ':=': Operator.equals,
+};
 
 abstract class Parser {
   static ListItemState parse(ListItemState model) {
@@ -17,33 +26,21 @@ abstract class Parser {
     updatedModel = updatedModel.copyWith(indent: parseIndent(model.fullText));
 
     Operator operator = Operator.none;
-
-    <String, Operator>{
-      ':-': Operator.subtract,
-      ':+': Operator.add,
-      ':/': Operator.divide,
-      ':*': Operator.multiply,
-      ':=': Operator.equals,
-    }.forEach((key, value) {
-      if (source.startsWith(key)) {
-        operator = value;
-      }
-    });
-
-    if (operator != Operator.none) {
-      source = ':${source.substring(2)}';
-    }
-
     num? number;
-    if (hasMatch(source)) {
-      var match = getMatch(source);
-      number = num.tryParse(match.substring(1));
-      if (number != null) {
-        updatedModel = updatedModel.copyWith(number: number);
-        if (operator == Operator.none) {
-          operator = Operator.add;
+
+    RegExpMatch? match = mathematicalExpressionRegex.firstMatch(source);
+
+    if (match != null) {
+      operator = operators[match.namedGroup("op")] ?? Operator.add;
+
+      if (operator != Operator.equals) {
+        number = num.tryParse(match.namedGroup("num") ?? "");
+
+        if (number != null) {
+          source = match.namedGroup("text")?.trim() ?? "";
         }
-        source = source.substring(getMatch(source).length).trim();
+      } else {
+        source = source.substring(match.namedGroup("op")?.length ?? 0);
       }
     }
 
@@ -54,16 +51,5 @@ abstract class Parser {
     );
 
     return updatedModel;
-  }
-
-  static bool hasMatch(String source) => expression.hasMatch(source);
-
-  static String getMatch(String source) {
-    final match = expression.firstMatch(source);
-    if (match != null) {
-      return source.substring(match.start, match.end);
-    } else {
-      return '';
-    }
   }
 }
