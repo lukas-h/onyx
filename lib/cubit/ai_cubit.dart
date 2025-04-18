9d6d788eb0ee4ab355838857e9f1b8dd85f58fd0
BenRyc4203 <fishs4203@gmail.com>
2025-04-19 09:53:19 +1000

    better cubit

--- lib/cubit/ai_cubit.dart
+++ lib/cubit/ai_cubit.dart
@@ -1,21 +1,145 @@
 import 'package:onyx/service/ai_service.dart';
 import 'package:replay_bloc/replay_bloc.dart';
 
-class AiCubit extends Cubit<List<OpenAiResponse>> {
-  final AiService service;
+import 'dart:async';
+import 'dart:convert';
 
-  AiCubit(super.initialState, {required this.service});
+import 'package:http/http.dart' as http;
 
-  Future<bool> request(String input) async {
-    final response = await service.request(input);
+class OpenAiResponse {
+  final int createdAt;
+  final Object? error;
+  final String id;
+  final Object? incompleteDetails;
+  final String? instructions;
+  final int? maxOutputTokens;
+  final Object metadata;
+  final String model;
+  final String object;
+  final List output;
+  final bool parallelToolCalls;
+  final String? previousResponseId;
+  final Object? reasoning;
+  final String status;
+  final int? temperature;
+  final Object? text;
+  final int? topP;
+  final String? truncation;
+  final Object usage;
+  final String user;
 
-    if (response != null) {
-      final newState = List.of(state, growable: true);
-      newState.add(response);
-      emit(newState);
-      return true;
+  OpenAiResponse({
+    required this.createdAt,
+    this.error,
+    required this.id,
+    this.incompleteDetails,
+    this.instructions,
+    this.maxOutputTokens,
+    required this.metadata,
+    required this.model,
+    required this.object,
+    required this.output,
+    required this.parallelToolCalls,
+    this.previousResponseId,
+    this.reasoning,
+    required this.status,
+    this.temperature,
+    this.text,
+    this.topP,
+    this.truncation,
+    required this.usage,
+    required this.user,
+  });
+
+  factory OpenAiResponse.fromJson(Map<String, dynamic> json) {
+    return OpenAiResponse(
+      createdAt: json['created_at'] as int,
+      error: json['error'] as Object?,
+      id: json['id'] as String,
+      incompleteDetails: json['incomplete_details'] as Object?,
+      instructions: json['instructions'] as String?,
+      maxOutputTokens: json['max_output_tokens'] as int?,
+      metadata: json['metadata'] as Object,
+      model: json['model'] as String,
+      object: json['object'] as String,
+      output: json['output'] as List,
+      parallelToolCalls: json['parallel_tool_calls'] as bool,
+      previousResponseId: json['previous_response_id'] as String?,
+      reasoning: json['reasoning'] as Object?,
+      status: json['status'] as String,
+      temperature: json['temperature'] as int?,
+      text: json['text'] as Object?,
+      topP: json['top_p'] as int?,
+      truncation: json['truncation'] as String?,
+      usage: json['usage'] as Object,
+      user: json['user'] as String,
+    );
+  }
+}
+
+class AiServiceState {
+  final String apiToken;
+  final String model;
+  final List<String> availableModels;
+
+  AiServiceState(
+    this.apiToken, {
+    this.model = "gpt-4.1-nano",
+    this.availableModels = const [],
+  });
+
+  AiServiceState copyWith({String? newApiToken, String? newModel, List<String>? newAvailableModels}) {
+    return AiServiceState(newApiToken ?? apiToken, model: newModel ?? model, availableModels: newAvailableModels ?? availableModels);
+  }
+}
+
+class AiServiceCubit extends Cubit<AiServiceState> {
+  AiServiceCubit({
+    required String apiToken,
+    String? model,
+  }) : super(AiServiceState(apiToken, model: model ?? "gpt-4.1-nano"));
+
+  set model(String newModel) {
+    if (state.availableModels.contains(newModel)) {
+      emit(state.copyWith(newModel: newModel));
     }
+  }
 
-    return false;
+  String get model {
+    return state.model;
+  }
+
+  set apiToken(String newApiToken) {
+    emit(state.copyWith(newApiToken: newApiToken));
+  }
+
+  String get apiToken {
+    return state.apiToken;
+  }
+
+  Future<void> updateModels() async {
+    // todo get models
+  }
+
+  List<String> get availableModels {
+    return state.availableModels;
+  }
+
+  Future<OpenAiResponse?> request(String input) async {
+    final url = Uri.https('api.openai.com', '/v1/responses');
+
+    final Map<String, String> headers = <String, String>{};
+    headers["Content-Type"] = "application/json";
+    headers["Authorization"] = "Barer $apiToken";
+
+    try {
+      final response = await http.post(url, headers: headers, body: {'model': model, 'input': input});
+
+      return OpenAiResponse.fromJson(jsonDecode(response.body));
+    } catch (e) {
+      // todo better logging
+      print(e);
+      return null;
+    }
   }
 }
