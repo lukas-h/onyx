072743953ef8d6d60e3f134fa876f22cc92547bb
Ryan Comerford <hello@ryancomerford.me>
2025-04-18 12:40:52 +1000

    Basic to markdown tests

--- test/page_model_test.dart
+++ test/page_model_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:onyx/store/page_store.dart';
 
 void main() {
-  group('Markdown Parser', () {
-    test('fails to parse empty string', () {
+  group('From Markdown', () {
+    test('', () {
       const markdownString = '';
 
       expect(() => PageModel.fromMarkdown(markdownString), throwsA(isA<Exception>()));
@@ -70,6 +70,77 @@ pretty neat''';
       expect(model.modified, DateTime.parse("2025-04-14T20:12:08.871912"));
       expect(model.uid, "Qpj17TkOKVffGz1");
       expect(model.fullText, ['content line', 'more content', 'so\$ %% \\n\\n symbols; wow!', 'pretty neat']);
+    });
+  });
+
+  group('To Markdown', () {
+    test('creates markdown for journal with no content', () {
+      final timeNow = DateTime.now();
+
+      final model = PageModel(title: "18/04/2025", created: DateTime.parse("2025-04-18T11:17:51.172975"), modified: timeNow, uid: "18/04/2025", fullText: ['']);
+
+      final markdown = model.toMarkdown();
+
+      expect(markdown, '''
+---
+title: 18/04/2025
+created: 2025-04-18T11:17:51.172975
+modified: ${timeNow.toIso8601String()}
+uid: 18/04/2025
+---
+
+
+''');
+    });
+
+    test('creates markdown for page with complex content', () {
+      final timeNow = DateTime.now();
+
+      final model = PageModel(
+          title: "Page with complex content",
+          created: DateTime.parse("2025-04-18T12:30:14.671637"),
+          modified: timeNow,
+          uid: "XYQvV-NwgD-6UJ_",
+          fullText: [
+            '# This is the main title',
+            '## And a kind of subheading thing',
+            ':+100 We start with a positive value',
+            ':-90 Then we minus',
+            ':*2',
+            ':/5 And times and divide',
+            ':= 9 <-- this number is unrelated to the equals!',
+            '* Now a list',
+            '* Another item',
+            '* And a third',
+            '-[ ] Unchecked item',
+            '-[x] Checked item',
+            '[[15/04/2025]] And a link to something else!',
+          ]);
+
+      final markdown = model.toMarkdown();
+
+      expect(markdown, '''
+---
+title: Page with complex content
+created: 2025-04-18T12:30:14.671637
+modified: ${timeNow.toIso8601String()}
+uid: XYQvV-NwgD-6UJ_
+---
+
+# This is the main title
+## And a kind of subheading thing
+:+100 We start with a positive value
+:-90 Then we minus
+:*2
+:/5 And times and divide
+:= 9 <-- this number is unrelated to the equals!
+* Now a list
+* Another item
+* And a third
+-[ ] Unchecked item
+-[x] Checked item
+[[15/04/2025]] And a link to something else!
+''');
     });
   });
 }
