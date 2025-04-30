e7ab292334826c5f6e3f4e2986a0836b179889a7
Ryan Comerford <hello@ryancomerford.me>
2025-04-30 12:32:00 +1000

    Add example version control methods to origin service base class

--- lib/service/origin_service.dart
+++ lib/service/origin_service.dart
@@ -2,37 +2,50 @@ import 'package:onyx/store/image_store.dart';
 import 'package:onyx/store/page_store.dart';
 
 abstract class OriginService {
+  void close();
+
+  // --- FAVORITES ---
+
   Future<List<String>> getFavorites();
-
-  Future<List<PageModel>> getPages();
-
-  void subscribeToPages();
-
-  Future<List<PageModel>> getJournals();
-
-  void subscribeToJournals();
-
-  Future<void> createPage(PageModel model);
-
-  Future<void> createJournal(PageModel model);
-
-  Future<void> updatePage(PageModel model);
-
-  Future<void> updateJournal(PageModel model);
-
-  Future<void> deletePage(String uid);
-
   Future<void> createFavorite(String uid);
-
   Future<void> deleteFavorite(String uid);
 
+  // --- GET ---
+
+  Future<List<PageModel>> getPages();
+  Future<List<PageModel>> getJournals();
+  Future<List<ImageModel>> getImages();
+
+  // --- CREATE ---
+
+  Future<void> createPage(PageModel model);
+  Future<void> createJournal(PageModel model);
   Future<void> createImage(ImageModel image);
 
+  // --- UPDATE ---
+
+  Future<void> updatePage(PageModel model);
+  Future<void> updateJournal(PageModel model);
+
+  // --- DELETE ---
+
+  Future<void> deletePage(String uid);
   Future<void> deleteImage(String uid);
 
-  Future<List<ImageModel>> getImages();
+  // --- WATCH CHANGES ---
+
+  void subscribeToPages();
+  void subscribeToJournals();
 
   void markConflictResolved();
 
-  void close();
+  // --- VERSION CONTROL ---
+
+  Future<List<PageModel>> getPageVersions();
+  PageModel usePageVersion();
+  void commitPage(PageModel model);
+
+  Future<List<PageModel>> getJournalVersions();
+  PageModel useJournalVersion(model);
+  void commitJournal(PageModel model);
 }
