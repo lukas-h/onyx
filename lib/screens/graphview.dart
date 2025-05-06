fbd36bfdae0e05f16b3a2fc21df70084601448ab
Rohan Mhadgut <rohamhad98@gmail.com>
2025-05-06 15:55:17 +1000

    Moved graph function to graph_cubit

--- lib/screens/graphview.dart
+++ lib/screens/graphview.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphview/GraphView.dart';
+import 'package:onyx/cubit/graph_cubit.dart';
 import 'package:onyx/cubit/navigation_cubit.dart';
 import 'package:onyx/cubit/page_cubit.dart';
-import 'package:onyx/editor/model.dart';
-import 'package:onyx/store/page_store.dart';
 
 class GraphViewScreen extends StatelessWidget {
   const GraphViewScreen({super.key});
@@ -20,8 +19,6 @@ class TreeViewPage extends StatefulWidget {
 }
 
 class _TreeViewPageState extends State<TreeViewPage> {
-  Map<String, Node> titleNode = {};
-  final List<Node> recusrionExist = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,17 +37,22 @@ class _TreeViewPageState extends State<TreeViewPage> {
                   Positioned.fill(
                     child: Align(
                       alignment: Alignment.center,
-                      child: GraphView(
-                        graph: graph,
-                        algorithm: SugiyamaAlgorithm(builder),
-                        paint: Paint()
-                          ..color = Colors.green
-                          ..strokeWidth = 1
-                          ..style = PaintingStyle.stroke,
-                        builder: (Node node) {
-                          String? key = titleNode.entries.firstWhere((entry) => entry.value == node).key;
-                          return recusrionExist.contains(node) ? nodeWithRecursionWidget(key) : nodeWidget(key);
-                        },
+                      child: BlocBuilder<GraphCubit,GraphState>(
+                        bloc: GraphCubit(context.read<PageCubit>()),
+                        builder: (context,state) {
+                          return GraphView(
+                            graph: state.graph,
+                            algorithm: SugiyamaAlgorithm(builder),
+                            paint: Paint()
+                              ..color = Colors.green
+                              ..strokeWidth = 1
+                              ..style = PaintingStyle.stroke,
+                            builder: (Node node) {
+                              String? key = state.titleNode.entries.firstWhere((entry) => entry.value == node).key;
+                              return state.recursionExist.contains(node) ? nodeWithRecursionWidget(key) : nodeWidget(key);
+                            },
+                          );
+                        }
                       ),
                     ),
                   ),
@@ -140,93 +142,11 @@ class _TreeViewPageState extends State<TreeViewPage> {
     );
   }
 
-  final Graph graph = Graph()..isTree = false;
   SugiyamaConfiguration builder = SugiyamaConfiguration();
 
   @override
   void initState() {
     super.initState();
-    final pattern = RegExp(r'\[\[(.*?)\]\]');
-    final cubit = context.read<PageCubit>();
-
-    // Journal add nodes code starts here
-    for (int i = 0; i < cubit.store.journals.length; i++) {
-      PageModel page = cubit.store.journals.values.elementAt(i);
-      if (page.uid.isNotEmpty && page.title.isNotEmpty && page.uid.contains('/')) {
-        String numericString = page.uid.replaceAll('/', '');
-        int numericDate = int.parse(numericString);
-        final node = Node.Id(numericDate);
-        titleNode[page.title] = node;
-        graph.addNode(node);
-      }
-    }
-    // Journal add nodes code ends here
-
-    // Page add node code starts here
-    for (int i = 0; i < cubit.store.pages.length; i++) {
-      PageModel page = cubit.store.pages.values.elementAt(i);
-      if (page.uid.isNotEmpty && page.title.isNotEmpty) {
-        int randomId = UniqueKey().hashCode;
-        final node = Node.Id(randomId);
-        titleNode[page.title] = node;
-        graph.addNode(node);
-      }
-    }
-    //Page add node code ends here
-
-    // Journal edges code starts here
-    for (int i = 0; i < cubit.store.journals.length; i++) {
-      PageModel page = cubit.store.journals.values.elementAt(i);
-      if (page.uid.isNotEmpty && page.title.isNotEmpty && page.uid.contains('/')) {
-        final pageState = PageState.fromPageModel(page, true);
-        for (int x = 0; x < pageState.items.length; x++) {
-          ListItemState item = pageState.items.elementAt(x);
-          final matches = pattern.allMatches(item.fullText);
-          if (matches.isNotEmpty) {
-            for (final match in matches) {
-              final content = match.group(1); // Extracts the content between [[ and ]]
-              if (titleNode.containsKey(content)) {
-                Node node1 = titleNode[page.title]!;
-                Node node2 = titleNode[content]!;
-                if (node1 == node2) {
-                  recusrionExist.add(node1);
-                } else {
-                  graph.addEdge(node1, node2, paint: Paint()..color = Colors.blue);
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    // Journal edges Code ends here
-
-    // Pages edges code start here
-    for (int i = 0; i < cubit.store.pages.length; i++) {
-      PageModel page = cubit.store.pages.values.elementAt(i);
-      if (page.uid.isNotEmpty && page.title.isNotEmpty) {
-        final pageState = PageState.fromPageModel(page, false);
-        for (int x = 0; x < pageState.items.length; x++) {
-          ListItemState item = pageState.items.elementAt(x);
-          final matches = pattern.allMatches(item.fullText);
-          if (matches.isNotEmpty) {
-            for (final match in matches) {
-              final content = match.group(1); // Extracts the content between [[ and ]]
-              if (titleNode.containsKey(content)) {
-                Node node1 = titleNode[page.title]!;
-                Node node2 = titleNode[content]!;
-                if (node1 == node2) {
-                  recusrionExist.add(node1);
-                } else {
-                  graph.addEdge(node1, node2, paint: Paint()..color = Colors.blue);
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    // Pages edges code ends here
 
     builder
       ..nodeSeparation = 30
